020f4e4525f58e712271852cbc11d038ff1c89db
Morgan Trudeau <morgantrudeau@hotmail.com>
2024-08-01 18:53:57 -0700

    Added stripe api key to app.json

--- app/hocs/withStripe.tsx
+++ app/hocs/withStripe.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { StripeProvider } from "@stripe/stripe-react-native";
+import { stripeApiKey } from "../../app.json";
 
 export const withStripe = <P extends {}>(Component: React.ComponentType<P>) => {
   return function WithStripe(props: P) {
     return (
       <StripeProvider
-        publishableKey="pk_test_51P5xHH076A0nkV3SRDnuCLj3BmRHmwlz6xsFcG8ORd5Gdc1tVzbsTJQKmKmmuL2A6W3nPDHTklD8oRpFmMmRloiI00JMw181zS"
+        publishableKey={stripeApiKey}
         urlScheme="delivfree" // required for 3D Secure and bank redirects
         // merchantIdentifier="merchant.com.{{YOUR_APP_NAME}}" // required for Apple Pay
       >
